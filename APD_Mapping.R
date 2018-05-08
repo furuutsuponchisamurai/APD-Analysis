a2c366eebefbb320921e93d0bd2b976005df57bc
furuutsuponchisamurai <25882627+furuutsuponchisamurai@users.noreply.github.com>
2018-05-08 16:43:14 -0500

    Basic mapping: crimes and district boundaries

--- APD_Mapping.R
+++ APD_Mapping.R
@@ -1,5 +1,55 @@
 library(tidyverse)
-library(magrittr)
-library(nominatim)
-library(ggmap)
-library(maps)
+library(reshape2)
+library(leaflet)
+library(jsonlite)
+data <- read_csv("geodata.csv")
+maxlat <- max(data$lat, na.rm=T)
+maxlong <- max(data$long, na.rm=T)
+minlat <- min(data$lat, na.rm=T)
+minlong <- min(data$long, na.rm=T)
+
+#register_google(key = "AIzaSyDoAf6cwt4aw_2iKxNN64C9tOeQ9ge8yMo")
+data <- read_csv("geodata.csv")
+clean_data <- data %>% filter(!is.na(long))
+aus_data <- filter(clean_data, -97.94 <= long, lat <=  30.52) %>% filter(long <= -97.56, 30.09 <= lat)
+
+geoJSON <- fromJSON("districts-processed-geoJSON.json")
+geojson <- readLines("austin-council-processed.geojson", warn = FALSE) %>%
+  paste(collapse = "\n") %>%
+  fromJSON(simplifyVector = FALSE)
+
+leaflet(aus_data) %>% setView(lng = -97.7341, lat = 30.2849, zoom = 10) %>%
+  addTiles() %>% addGeoJSON(geojson) %>% addTiles() %>% addMarkers(clusterOptions = markerClusterOptions())
+# leaflet(aus_data) %>% addTiles() %>% addMarkers(clusterOptions = markerClusterOptions())
+
+# {
+#   "type": "Feature",
+#   "properties": {"district_id": "7", "council_representative": "Leslie Poole"},
+#   "geometry": {
+#     "type": "MultiPolygon",
+#     "coordinates": [[
+#       [-104.05, 48.99],
+#       [-97.22,  48.98],
+#       [-96.58,  45.94],
+#       [-104.03, 45.94],
+#       [-104.05, 48.99]
+#       ]]
+#   }
+# }
+# geodata <- read_csv("city-council.csv")
+# substrRight <- function(x, n){
+#   substr(x, nchar(x)-n+1, nchar(x))
+# }
+# rexp <- "^(\\w+)\\s?(.*)$"
+# nexp <- "(-[0-9]{2}\\.[0-9]+)\\s([0-9]{2}\\.[0-9]+)"
+# gdata <-  tibble(DISTRICT=geodata$COUNCIL_DISTRICT, SHAPE=sub(rexp,"\\1",geodata$the_geom), COORDS=sub(rexp,"\\2",geodata$the_geom))
+# gdata <- mutate(gdata, locs = gsub("\\,", "]\\,", COORDS))
+# gdata <- mutate(gdata, locs = gsub(nexp, "\\1, \\2", locs))
+# gdata <- mutate(gdata, locs = gsub("\\(", "[", locs))
+# gdata <- mutate(gdata, locs = gsub(")", "]", locs))
+# gdata <- mutate(gdata, locs = gsub("-", "[-", locs))
+# gdata <- mutate(gdata, locs = paste(locs, "]", sep = ""))
+# d <- substrRight(gdata$locs[1], 6)
+# 
+# js <- gdata$locs[3]
+# gdata <- mutate(gdata, locs = fromJSON(locs))
---
+++ csvToGeoJSON.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  7 23:16:35 2018
+
+@author: AK47
+"""
+#Create geoJSON for R leaflet app
+
+import pandas as pd, numpy as np, json, re
+
+df = pd.read_csv("city-council.csv")[['COUNCIL_DISTRICT','the_geom']]
+
+d1 = df['the_geom'][0]
+
+regex = r"(-[0-9]{2}\.[0-9]+)\s([0-9]{2}\.[0-9]+)"
+begex = r"\("
+cegex = r"\)"
+
+subst = "[\\1, \\2]"
+bsubst = "["
+csubst = "]"
+
+result = re.sub(cegex, csubst, re.sub(begex, bsubst, re.sub(regex, subst, d1, 0, re.MULTILINE), 0, re.MULTILINE), 0, re.MULTILINE)
+result = result.split(" ", maxsplit=1)
+
+def process(data):
+    result = re.sub(cegex, csubst, re.sub(begex, bsubst, re.sub(regex, subst,
+                    data, 0, re.MULTILINE), 0, re.MULTILINE),
+                     0, re.MULTILINE).split(" ", maxsplit=1)[1]
+    
+    return result
+
+df = df.assign(**{'SHAPE': df.the_geom.apply(lambda x: x.split(" ", maxsplit=1)[0]), "SHAPE_DATA": df.the_geom.apply(lambda x: process(x))})
+
+def mkdict(_id, shape, coords):
+    # Creates geoJson style dict needed for leaflet 
+    
+    shape = shape.lower()
+    s1, s2 = shape[0:5], shape[5:]
+    shape = ''.join([s1.capitalize(), s2.capitalize()])
+    dct = {"type": "Feature",
+           "properties": {"district_id": _id},
+           "geometry": {"type": shape,
+                        "coordinates": json.loads(coords)
+                        }
+           }
+    
+    
+    return dct
+
+districts = []
+
+for (idx, row) in df.iterrows():
+    dct = mkdict(str(row.loc['COUNCIL_DISTRICT']), row.loc['SHAPE'], row.loc['SHAPE_DATA'])
+    districts.append(dct)
+    
+with open('austin-council-processed.geojson', 'w') as jsonfile:
+    json.dump(districts, jsonfile)
