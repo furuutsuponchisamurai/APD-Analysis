9e30f5b406d925a8114635f067cac022d074673d
furuutsuponchisamurai <25882627+furuutsuponchisamurai@users.noreply.github.com>
2018-08-05 10:20:41 -0500

    added graph for districts

--- APD_Analysis.R
+++ APD_Analysis.R
@@ -1,10 +1,11 @@
 # Analysis of crime data
 library(tidyverse)
 
-crime_data <- read_delim("council-crime-2016.tsv", "\t")
+crime_data <- read_delim("council-crime-2016.tsv", "\t") %>% mutate(district_id = as.character(district_id))
 top_15_incidents <- crime_data %>% count(crime_cat, sort = TRUE) %>% head(15)
 
 # Filtering join for crime_data
 filtered_crime <- crime_data %>% semi_join(top_15_incidents)
 # visiualize crime counts for top 15
-# ggplot(data = top_15_incidents) + geom_point(mapping = aes(x = crime_cat, y = n))
+ggplot(data = filtered_crime) + geom_bar(mapping = aes(x = crime_cat))
+ggplot(data = crime_data) + stat_count(mapping = aes(x = district_id))
